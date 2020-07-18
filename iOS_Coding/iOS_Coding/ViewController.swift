c02472de9f843d93b393abf1fa7a22b0942aed9c
Akash Sheelavant <ASheelavant@walmart.com>
2020-07-18 22:00:08 +0530

    Adding initial project.

---
+++ iOS_Coding/iOS_Coding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Coding
+//
+//  Created by Akash Sheelavant - Vendor on 7/18/20.
+//  Copyright © 2020 Akash Sheelavant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
